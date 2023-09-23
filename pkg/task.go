13999ce61c3a7e441e84f484bfea7f2c86b46970
Kijima Daigo <norimaking777@gmail.com>
2023-09-23 14:30:47 +0900

    feat: 依存関係をチェックして満たしていなかったら中断する

--- pkg/task.go
+++ pkg/task.go
@@ -7,25 +7,28 @@ import (
 type Task struct {
 	name     string
 	status   statusText
+	depsCmds []boolFunc
 	instCmds []errorFunc
 }
 
 type statusText string
 
 const (
-	waitExecute     = statusText("wait Execute")
-	successInstall  = statusText("Success install")
-	failInstall     = statusText("Fail install")
-	notMet          = statusText("Dependencies not met")
-	alreadyAchieved = statusText("Already achieved")
+	waitExecuteST     = statusText("wait Execute")
+	successInstallST  = statusText("Success install")
+	failInstallST     = statusText("Fail install")
+	notMetST          = statusText("Dependencies not met, skip")
+	alreadyAchievedST = statusText("Already achieved, skip")
 )
 
+type boolFunc func() bool
 type errorFunc func() error
 
-func NewTask(name string, instCmds []errorFunc) Task {
+func NewTask(name string, depsCmds []boolFunc, instCmds []errorFunc) Task {
 	t := Task{
 		name:     name,
-		status:   waitExecute,
+		status:   waitExecuteST,
+		depsCmds: depsCmds,
 		instCmds: instCmds,
 	}
 	return t
@@ -34,14 +37,40 @@ func NewTask(name string, instCmds []errorFunc) Task {
 func (t *Task) Run() {
 	fmt.Printf("[%s]\n", t.name)
 
-	for _, cmd := range t.instCmds {
-		err := cmd()
-		if err != nil {
-			t.status = failInstall
-		} else {
-			t.status = successInstall
+	procs := []func() bool{
+		t.processDeps,
+		t.processInst,
+	}
+
+	for _, proc := range procs {
+		ok := proc()
+		if !ok {
+			break
 		}
 	}
 
 	fmt.Printf("=> %s\n", t.status)
+}
+
+func (t *Task) processDeps() bool {
+	for _, cmd := range t.depsCmds {
+		ok := cmd()
+		if !ok {
+			t.status = notMetST
+			return false
+		}
+	}
+	return true
+}
+func (t *Task) processInst() bool {
+	for _, cmd := range t.instCmds {
+		err := cmd()
+		if err != nil {
+			t.status = failInstallST
+			return false
+		} else {
+			t.status = successInstallST
+		}
+	}
+	return true
 }
--- pkg/task_test.go
+++ pkg/task_test.go
@@ -10,6 +10,25 @@ func TestTaskRun(t *testing.T) {
 
 	task := NewTask(
 		"Run uname command",
+		[]boolFunc{},
+		[]errorFunc{testfunc, testfunc},
+	)
+	task.Run()
+}
+
+func TestTaskNotMet(t *testing.T) {
+	depsfunc := func() bool {
+		return IsExistCmd("not_found_cmd")
+	}
+
+	testfunc := func() error {
+		err := Run("uname")
+		return err
+	}
+
+	task := NewTask(
+		"Run uname command",
+		[]boolFunc{depsfunc},
 		[]errorFunc{testfunc, testfunc},
 	)
 	task.Run()
